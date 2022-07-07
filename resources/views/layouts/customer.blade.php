23f38a27f1942a82a3fad607d61c9129811bdf21
Shameerk288 <shameerk288@gmail.com>
2022-07-07 06:13:36 -0700

    commit

--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -132,7 +132,7 @@
                 <div class="row">
                     <div class="col-lg-2 col-md-2">
                         <div class="header__logo">
-                            <a href="/"><img src="{{ asset('assets/images/logo.jpg') }}" alt="Company Logo"></a>
+                            <a href="/"><img src="{{ asset('assets/images/logo.JPG') }}" alt="Company Logo"></a>
 
                         </div>
                     </div>
